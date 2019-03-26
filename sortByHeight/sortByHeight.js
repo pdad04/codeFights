65a18925e2765fc43b5f96cce9a490ce91c62bf9
Andre <andre_dean@me.com>
2019-03-25 21:15:16 -0700

    Updated to swap snake_case variable names to CamalCase variable names.

--- sortByHeight/sortByHeight.js
+++ sortByHeight/sortByHeight.js
@@ -1,23 +1,23 @@
 function sortByHeight(a) {
   var trees = [];
-  var sorted_by_height = [];
+  var sortedByHeight = [];
 
   a.filter(function(element,index){
     if(element === -1) {
       trees.push(index);
     }else {
-      sorted_by_height.push(element);
+      sortedByHeight.push(element);
     }
   });
 
-  sorted_by_height.sort(function(a,b){
+  sortedByHeight.sort(function(a,b){
     return a-b;
   });
 
 
   for(var i = 0; i < trees.length; i++) {
-    sorted_by_height.splice(trees[i],0,-1);
+    sortedByHeight.splice(trees[i],0,-1);
   }
 
-  return sorted_by_height;
+  return sortedByHeight;
 }
